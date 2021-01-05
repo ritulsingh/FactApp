9203092c9da487e10ab33240ddc5667cb053436d
ritulsingh <ritulsingh00@gmail.com>
2021-01-05 23:25:17 +0530

    Facts App

---
+++ app/src/main/java/com/ritulsingh/factsapp/Facts.java
@@ -0,0 +1,33 @@
+package com.ritulsingh.factsapp;
+
+public class Facts {
+
+    public static String[] getFacts(){
+        return new String[] {
+                "Animals that lay eggs don't have belly buttons.",
+                "Beavers can hold their breath for 45 minutes under water.",
+                "Slugs have four noses.",
+                "Camels have three eyelids.",
+                "A honey bee can fly at 15mph.",
+                "A queen bee can lay 800-1,500 eggs per day.",
+                "A bee has five eyelids.",
+                "The average speed of a housefly is 4.5 mph.",
+                "Mosquitoes are attracted to people who just ate bananas.",
+                "Flamingos turn pink from eating shrimp.",
+                "Emus and kangaroos cannot walk backward.",
+                "The world's termites outweigh the world's humans about 10 to 1.",
+                "A hummingbird weighs less than a penny.",
+                "Alaska has the highest percentage of people who walk to work.",
+                "On average a human being will spend up to 2 weeks kissing in his/her lifetime.",
+                "Termites eat through wood two times faster when listening to rock music!",
+                "If you keep a goldfish in a dark room it will eventually turn white.",
+                "A snail breathes through its foot.",
+                "It is possible to lead a cow up stairs but not down.",
+                "Elephants are capable of swimming 20 miles per day.",
+                "Elephants are the only mammals that can't jump.",
+                "There are 63,360 inches in a mile.",
+                "About 11% of the people in the world are left-handed.",
+                "1/4 of the bones in your body are in your feet."
+        };
+    }
+}
